c337c49dc2443a3766fed4afe4c0acde353fe179
Siwat Sirichai <siwat@siwatinc.com>
2022-04-04 18:11:46 +0700

    remove root password

--- initializedb.sh
+++ initializedb.sh
@@ -4,7 +4,7 @@ chmod 777 -v /run/mysqld
 mkdir -p /config/database
 if [ -z "$(ls -A /config/database)" ]; then
    echo "Initializing . . ."
-   mysqld --initialize --datadir /config/database
+   mysqld --initialize-insecure --datadir /config/database
    mysqld --datadir /config/database &
    cat /software/initializedb.sql | mysql
 else
